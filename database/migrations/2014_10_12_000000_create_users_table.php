84ca6d7fba676ad5ae1f5b1f5d1d5531d206d439
Antunes Domingos <antunesjunior438@gmail.com>
2022-06-28 02:32:40 +0100

    Migratios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('user_name')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('picture');
+            $table->string('web')->nullable();
+            $table->text('bio')->nullable();
+            $table->string('company')->nullable();
+            $table->string('city')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
