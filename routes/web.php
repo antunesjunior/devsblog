1dfef589edec2541cb34a078645dd394c1c39777
Antunes Domingos <antunesjunior438@gmail.com>
2022-07-30 14:51:28 +0100

    refact: posts routes

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('posts', PostController::class)->except([
         'destroy'
     ]);
-    Route::get('/posts/user/{userId}/published', [PostController::class, 'published'])->name('posts.published');
-    Route::get('/posts/user/{userId}/drafts', [PostController::class, 'drafts'])->name('posts.draft');
+    Route::get('/posts/{username}/published', [PostController::class, 'published'])->name('posts.published');
+    Route::get('/posts/{username}/drafts', [PostController::class, 'drafts'])->name('posts.draft');
     Route::get('/posts/{id}/delete', [PostController::class, "destroy"])->name('posts.destroy');
 });
