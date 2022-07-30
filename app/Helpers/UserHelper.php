3ba77b8ab3915f04b17f75c114dcabcf70b88c4b
Antunes Domingos <antunesjunior438@gmail.com>
2022-07-30 14:20:25 +0100

    refact: username methode

--- app/Helpers/UserHelper.php
+++ app/Helpers/UserHelper.php
@@ -7,12 +7,11 @@ use Illuminate\Support\Str;
 
 class UserHelper
 {
-    public static function generateUserName($name)
+    public static function generateUserName($username)
     {
-        $username = '@'.Str::lower($name);
-
-        if(User::where('username', '=', $username)->exists()){
-            $uniqueUserName = $username.rand();
+        $username = Str::slug($username, '');
+        if (User::where('username', $username)->exists()){
+            $uniqueUserName = $username.rand(1, 1000);
             $username = self::generateUserName($uniqueUserName);
         }
         return $username;
